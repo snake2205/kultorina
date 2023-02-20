66e42ea5cce28b65c38cabb53f51c1f12bd99931
snake2205 <mikshenrijs@gmail.com>
2023-02-20 10:50:36 +0200

    111

---
+++ frontend/src/pages/components/forms/make_quiz_form.jsx
@@ -0,0 +1,49 @@
+﻿import React from "react";
+import { Link, Outlet } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useToken } from "../hooks/useToken";
+import { useState, useEffect } from "react";
+import { return_error } from "../errors/errorHandling";
+import { Navigate } from "react-router-dom";
+
+function Signup_Form() {
+    const { register, handleSubmit } = useForm();
+    const { token, setToken } = useToken();
+    const [error, setError] = useState(null);
+
+    const onSubmit = (data, e) => {
+        const payload = new FormData();
+        payload.append(`username`, data.username);
+        payload.append(`password`, data.password);
+        payload.append(`email`, data.email);
+        axios.post("http://127.0.0.1:8000/auth/signup", payload)
+            .then((res) => { setToken(res.data); setError(<Navigate to="/" />); })
+            .catch((err) => {
+                setError(return_error(err));
+            })
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Elektroniskā pasta adrese:</label><br />
+                <select id="field" name="field" size="2">
+                    <option value="Foto">Fotogrāfijas</option>
+                    <option value="Audio">Mūzika</option>
+                </select>
+                <label>Lietotājvārds:</label><br />
+                <input {...register("username")} type="text" /><br />
+                <label>Parole:</label><br />
+                <input {...register("password")} type="text" /><br />
+                <button>Informācija ir ievadīta!</button>
+            </form>
+            <p>{error}</p>
+        </>
+
+    )
+}
+
+
+
+
+export default Signup_Form;
---
+++ frontend/src/pages/quizMaker/make_quiz.jsx
@@ -0,0 +1,11 @@
+import Make_Quiz_Form from "../components/forms/make_quiz_form";
+function Make_Quiz() {
+    return (
+        <>
+            <h1>Make Quiz</h1>
+            <Make_Quiz_Form />
+        </>
+    );
+}
+
+export default Make_Quiz;
