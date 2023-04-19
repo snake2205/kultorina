c40b13b26f86f35ec46903bc01b2e79ec803e849
DESKTOP-7KP2K2A\Home <davis.freim@inbox.lv>
2023-04-19 20:38:29 +0300

    Nomainītas krāsas, uzliks bg, home texts sapists, nemaina pozīciju

--- frontend/src/pages/components/design/navbar.jsx
+++ frontend/src/pages/components/design/navbar.jsx
@@ -6,12 +6,13 @@ function NavBar() {
     return (
         <div className="container-fluid vh-100 d-flex flex-column">
             <div className="row" style={{ height: "7%" }}>
-                <div className="d-flex flex-column bg-dark align-items-end bordermain">
-                    <img src={require("../images/klogo.png")} className="img-fluid my-auto" width="50px"></img>
+                <div className="d-flex flex-column bg-dark bordermain" align="start">
+                        <img src={require("../images/klogo.png")} className="img-fluid my-auto" width="3.3%"></img>
                 </div>
             </div>
             <div className="row flex-grow-1" style={{ maxHeight: "93%" }}>
-                <div className="col-12 col-sm-10 overflow-auto bg-light d-flex h-100">
+                <div className="col-12 col-sm-10 overflow-auto bodymainbg d-flex h-100">
+
                     <Outlet />
                     </div>
                 <Sidebar />
